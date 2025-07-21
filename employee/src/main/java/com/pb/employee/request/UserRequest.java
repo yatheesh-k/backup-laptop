d75238985c5ab2366f9ea8d07baa3fe11dc2ab1b
Gopi-vbrsit <gopi.g@pathbreakertech.in>
2025-07-21 16:16:46 +0530

    added roles field in user registration

--- employee/src/main/java/com/pb/employee/request/UserRequest.java
+++ employee/src/main/java/com/pb/employee/request/UserRequest.java
@@ -1,5 +1,6 @@
 package com.pb.employee.request;
 
+import com.pb.employee.validations.RoleValidation;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -7,6 +8,7 @@ import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import javax.annotation.Nullable;
+import java.util.List;
 
 @Getter
 @Setter
@@ -36,5 +38,8 @@ public class UserRequest {
     private String userType;
 
     private String department;
+
+    @RoleValidation
+    private List<String> roles;
 }
 
