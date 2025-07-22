fa6c9ad5b56a181564e0a0d9943de1c51a20fcde
Gopi-vbrsit <gopi.g@pathbreakertech.in>
2025-07-22 14:55:11 +0530

    updated user role

--- employee/src/main/java/com/pb/employee/serviceImpl/UserServiceImpl.java
+++ employee/src/main/java/com/pb/employee/serviceImpl/UserServiceImpl.java
@@ -9,7 +9,6 @@ import com.pb.employee.exception.EmployeeException;
 import com.pb.employee.exception.ErrorMessageHandler;
 import com.pb.employee.opensearch.OpenSearchOperations;
 import com.pb.employee.persistance.model.*;
-import com.pb.employee.request.UpdateRolesRequest;
 import com.pb.employee.request.UserRequest;
 import com.pb.employee.request.UserUpdateRequest;
 import com.pb.employee.response.EmployeeResponse;
@@ -223,7 +222,7 @@ public class UserServiceImpl implements UserService {
 
             UserEntity existingUser = dao.get(existingUsers.stream().findFirst().get().getId(), companyName).get();
 
-            if (userUpdateRequest.getDepartment() != null) {
+            if (userUpdateRequest.getDepartment() != null && !userUpdateRequest.getDepartment().isEmpty()) {
                 departmentEntity = openSearchOperations.getDepartmentById(userUpdateRequest.getDepartment(), null, index);
                 if (departmentEntity == null) {
                     log.error("Department not found: {}", userUpdateRequest.getDepartment());
@@ -236,6 +235,7 @@ public class UserServiceImpl implements UserService {
             if (userUpdateRequest.getUserType().equals(existingUser.getUserType()) &&
                     userUpdateRequest.getFirstName().equals(existingUser.getFirstName()) &&
                     userUpdateRequest.getLastName().equals(existingUser.getLastName()) &&
+                    userUpdateRequest.getRoles().equals(existingUser.getRoles())&&
                     Objects.equals(userUpdateRequest.getDepartment(), existingUser.getDepartment())) {
                 throw new EmployeeException(ErrorMessageHandler.getMessage(EmployeeErrorMessageKey.NO_CHANGES_DONE), HttpStatus.BAD_REQUEST);
             }
@@ -284,46 +284,6 @@ public class UserServiceImpl implements UserService {
             );
         }
     }
-
-
-    @Override
-    public ResponseEntity<?> updateUserRoles(String companyName, String id, UpdateRolesRequest updatePayload) throws EmployeeException {
-        String index;
-        try {
-            index = ResourceIdUtils.generateCompanyIndex(companyName);
-            CompanyEntity companyEntity = openSearchOperations.getCompanyByCompanyName(companyName, Constants.INDEX_EMS);
-            if (companyEntity == null){
-                log.error("Exception while fetching the company calendar details");
-                throw new EmployeeException(ErrorMessageHandler.getMessage(EmployeeErrorMessageKey.COMPANY_NOT_EXIST), HttpStatus.NOT_FOUND);
-            }
-
-            Collection<UserEntity> existingUsers = dao.getUsers(companyName, id, companyEntity.getId(), null);
-            if (existingUsers == null) {
-                log.error("User not found in this company {}", companyName);
-                throw new EmployeeException(String.format(ErrorMessageHandler.getMessage(EmployeeErrorMessageKey.USER_NOT_FOUND),companyName), HttpStatus.NOT_FOUND);
-            }
-
-            UserEntity existingUser = dao.get(existingUsers.stream().findFirst().get().getId(), companyName).get();
-            if (updatePayload.getRoles().equals(existingUser.getRoles())) {
-                log.error("No changes made to user roles for user {}", existingUser.getId());
-                throw new EmployeeException(ErrorMessageHandler.getMessage(EmployeeErrorMessageKey.NO_CHANGES_DONE), HttpStatus.BAD_REQUEST);
-            }
-            UserEntity updatedData = objectMapper.convertValue(updatePayload, UserEntity.class);
-            BeanUtils.copyProperties(updatedData, existingUser, getNullPropertyNames(updatedData));
-            dao.save(existingUser, companyName);
-
-            return new ResponseEntity<>(
-                    ResponseBuilder.builder().build().createSuccessResponse(Constants.SUCCESS), HttpStatus.OK);
-
-        } catch (EmployeeException employeeException) {
-            log.error("Error during user update: {}", employeeException.getMessage());
-            throw employeeException;
-        } catch (Exception exception) {
-            log.error("Error during user update: {}", exception.getMessage());
-            throw new EmployeeException(ErrorMessageHandler.getMessage(EmployeeErrorMessageKey.UNABLE_SAVE_USER), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
 
     private String[] getNullPropertyNames(Object source) {
         final BeanWrapper src = new BeanWrapperImpl(source);
